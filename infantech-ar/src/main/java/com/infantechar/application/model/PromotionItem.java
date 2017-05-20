ebe717c3e19c8819b2fe1d22f89f9c159d2cf98a
jan.d.bobis <jan.d.bobis@UPPT-51412XW-LX.dir.svc.accenture.com>
2017-05-20 09:10:27 +0800

    commit

--- infantech-ar/src/main/java/com/infantechar/application/model/PromotionItem.java
+++ infantech-ar/src/main/java/com/infantechar/application/model/PromotionItem.java
@@ -40,82 +40,124 @@ public class PromotionItem {
 	@Column(name="validitydateend")
 	private Date validityDateEnd;
 	
-	@Column(name="respawn")
-	private int reSpawn;
-	
 	@Column(name="latitude")
 	private double latitude;
 	
 	@Column(name="longitude")
 	private double longitude;
 	
+	@Column(name="respawn")
+	private int reSpawn;
+	
+	@Column(name="enabled")
+	private int enabled;
+	
+	@Column(name="claimed")
+	private int claimed;
+	
 	@Column(name="userid")
 	private long userId;
-	
-	
+
 	public long getPromotionItemId() {
 		return promotionItemId;
 	}
+
 	public void setPromotionItemId(long promotionItemId) {
 		this.promotionItemId = promotionItemId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getPromotionType() {
 		return promotionType;
 	}
+
 	public void setPromotionType(int promotionType) {
 		this.promotionType = promotionType;
 	}
+
 	public long getMerchantId() {
 		return merchantId;
 	}
+
 	public void setMerchantId(long merchantId) {
 		this.merchantId = merchantId;
-	}	
+	}
+
 	public Date getValidityDateStart() {
 		return validityDateStart;
 	}
+
 	public void setValidityDateStart(Date validityDateStart) {
 		this.validityDateStart = validityDateStart;
 	}
+
 	public Date getValidityDateEnd() {
 		return validityDateEnd;
 	}
+
 	public void setValidityDateEnd(Date validityDateEnd) {
 		this.validityDateEnd = validityDateEnd;
 	}
-	public int getReSpawn() {
-		return reSpawn;
-	}
-	public void setReSpawn(int reSpawn) {
-		this.reSpawn = reSpawn;
-	}
+
 	public double getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
+
 	public double getLongitude() {
 		return longitude;
 	}
+
 	public void setLongitude(double longitude) {
 		this.longitude = longitude;
 	}
+
+	public int getReSpawn() {
+		return reSpawn;
+	}
+
+	public void setReSpawn(int reSpawn) {
+		this.reSpawn = reSpawn;
+	}
+
+	public int getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(int enabled) {
+		this.enabled = enabled;
+	}
+
+	public int getClaimed() {
+		return claimed;
+	}
+
+	public void setClaimed(int claimed) {
+		this.claimed = claimed;
+	}
+
 	public long getUserId() {
 		return userId;
 	}
+
 	public void setUserId(long userId) {
 		this.userId = userId;
 	}	
